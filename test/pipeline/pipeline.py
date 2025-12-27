28ac1ca3fdc1dfb2559406e03404e35fdc1f4bfe
Zeeshan Khan <zeeshankhan0330@gmail.com>
2025-12-27 18:47:19 +0000

    jupyter

--- test/pipeline/pipeline.py
+++ test/pipeline/pipeline.py
@@ -1,5 +1,7 @@
+import pandas as pd
 import sys
-print("arguments", sys.argv)
 
-day = int(sys.argv[1])
-print(f"Running pipeline for day {day}")
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_day_{sys.argv[1]}.parquet")
